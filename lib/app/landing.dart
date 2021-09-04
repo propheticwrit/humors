fc5fdb31f145983897098bf1a95d898564290649
propheticwrit <job@vitalflohealth.com>
2021-09-03 20:00:28 -0400

    Firebase setup and google sign in

---
+++ lib/app/landing.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:humors/app/sign_in/ui/sign_in.dart';
+import 'package:humors/services/auth.dart';
+import 'package:provider/provider.dart';
+
+import 'base/ui/home.dart';
+
+class LandingPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBase>(context, listen: false);
+    return StreamBuilder<User?>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final User? user = snapshot.data;
+          if (user == null) {
+            return SignInPage.create(context);
+          }
+          return HomePage();
+        }
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:humors/services/auth.dart';
+import 'package:provider/provider.dart';
+
+import 'app/landing.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Humors());
+}
+
+class Humors extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider<AuthBase>(
+      create: (context) => Auth(),
+      child: MaterialApp(
+        title: 'Mood',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: LandingPage(),
+      ),
+    );
+  }
+}
