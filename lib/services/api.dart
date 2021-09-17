589e61777f821c4390f90a8eb432884a30c94526
propheticwrit <job@vitalflohealth.com>
2021-09-16 21:29:28 -0400

    Authenticate method sends the authToken to the social auth endpoint in the Mood api.  The values returned are dropped into the model APIUser.
    login method returns a Stream using the fromFuture method to update the home page when a response is received.

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,50 +1,66 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
 import 'package:humors/app/models/category.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+import 'api_path.dart';
+import 'auth.dart';
+import 'models/api_user.dart';
 
 abstract class Connector {
-  Future<void> addCategory(Category category);
+
+  APIUser? get apiUser;
+
+  Stream<APIUser?> login();
 }
 
 class MoodConnector implements Connector {
 
-  // var client = http.Client();
-  // final _token = '73458d6dca8be781acf2ded11b0ecc3c3f23b204';
-  // final String _url = 'api.clueo.net';
+  var client = http.Client();
 
-  MoodConnector({required String uid}) : assert(uid != null);
+  APIUser? _apiUser;
 
-  final String uid = '';
+  @override
+  APIUser? get apiUser => _apiUser;
 
-  Future<void> addCategory(Category category) async {
-    category.toJson();
-    print('adding category');
-  //  get future from api call using $uid
+  Stream<APIUser?> login() => Stream.fromFuture(authenticate());
+
+  Future<APIUser?> authenticate() async {
+
+    final prefs = await SharedPreferences.getInstance();
+    final authToken = prefs.getString('authToken') ?? null;
+
+    String username = '';
+    String email = '';
+    String refreshToken = '';
+    String accessToken = '';
+    if ( authToken != null ) {
+      var uri = Uri.http(APIPath.url, APIPath.login());
+      String body = "{\"auth_token\": \"${authToken}\"}";
+      final response = await http.post(
+          uri, headers: {'Content-Type': 'application/json'}, body: body);
+
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+      print('json response: ${jsonResponse}');
+
+      if (jsonResponse.isNotEmpty && jsonResponse.containsKey('username')) {
+        username = jsonResponse['username'];
+        email = jsonResponse['email'];
+        if (jsonResponse.containsKey('tokens')) {
+          refreshToken = jsonResponse['tokens']['refresh'];
+          accessToken = jsonResponse['tokens']['access'];
+        }
+        return APIUser(username, email, refreshToken, accessToken);
+      } else {
+        throw new FormatException('Invalid response format');
+      }
+    }
   }
-  // Future<Categories> fetchCategories() async {
-  //   var uri = Uri.http(_url, '/api/category');
-  //   final response = await http.get(uri, headers: {'Authorization': 'Token ${_token}'});
-  //
-  //   print(response.body.toString());
-  //   if (response.statusCode == 200) {
-  //     // If the call to the server was successful, parse the JSON
-  //     return Categories.fromJson(json.decode(response.body));
-  //   } else {
-  //     // If that call was not successful, throw an error.
-  //     throw Exception('Failed to load categories');
-  //   }
-  // }
-  //
-  // Future<Survey> fetchSurvey(int surveyId) async {
-  //   var uri = Uri.http(_url, '/api/survey/${surveyId}');
-  //   final response = await http.get(uri, headers: {'Authorization': 'Token ${_token}'});
-  //
-  //   print(response.body.toString());
-  //   if (response.statusCode == 200) {
-  //     // If the call to the server was successful, parse the JSON
-  //     return Survey.fromJson(json.decode(response.body));
-  //   } else {
-  //     // If that call was not successful, throw an error.
-  //     throw Exception('Failed to load survey');
-  //   }
-  // }
+
+  // TODO: Token deactivates after a 15 minute time period but may want to deactivate sooner
+  Future<void>? logout() {
+    return null;
+  }
 }
